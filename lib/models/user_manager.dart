2c33e8b4ea64c8ae83442ea0d7181c92fd133cb0
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2023-09-11 16:01:41 -0300

    Carregando Usuário

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, unused_local_variable
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -8,19 +8,28 @@ import 'package:loja_virtual/helpers/firebase_errors.dart';
 import 'user.dart';
 
 class UserManager extends ChangeNotifier {
+  UserManager() {
+    _loadCurrentUser();
+  }
+
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  bool loading = false;
+  User? user;
+
+  bool _loading = false;
+  bool get loading => _loading;
 
   Future<void> signIn(
       {required UserModel user,
       required Function onFail,
       required Function onSuccess}) async {
-    setLoading(true);
+    loading = true;
 
     try {
       final result = await _auth.signInWithEmailAndPassword(
           email: user.email!, password: user.password!);
+
+      this.user = result.user;
 
       onSuccess();
     } on FirebaseAuthException catch (e) {
@@ -28,11 +37,20 @@ class UserManager extends ChangeNotifier {
       onFail(getErrorString(e.code));
     }
 
-    setLoading(false);
+    loading = false;
   }
 
-  void setLoading(bool value) {
-    loading = value;
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  void _loadCurrentUser() async {
+    final currentUser = _auth.currentUser;
+
+    if (currentUser != null) {
+      user = currentUser;
+    }
     notifyListeners();
   }
 }
