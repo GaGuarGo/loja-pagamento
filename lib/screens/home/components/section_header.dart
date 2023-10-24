2747f557d5c2f888bc6557c25cff71457566aaae
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2023-10-24 17:20:44 -0300

    Editando Título e Removendo Seções

--- lib/screens/home/components/section_header.dart
+++ lib/screens/home/components/section_header.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_icon_button.dart';
+import 'package:loja_virtual/models/home_manager.dart';
 import 'package:loja_virtual/models/section.dart';
+import 'package:provider/provider.dart';
 
 class SectionHeader extends StatelessWidget {
   final Section section;
@@ -7,13 +10,43 @@ class SectionHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Text(
-        section.name ?? "",
-        style: const TextStyle(
-            color: Colors.white, fontWeight: FontWeight.w800, fontSize: 18),
-      ),
-    );
+    final homeManager = context.watch<HomeManager>();
+    if (homeManager.editing) {
+      return Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              initialValue: section.name,
+              decoration: const InputDecoration(
+                labelText: 'Título:',
+                labelStyle: TextStyle(color: Colors.white),
+                isDense: true,
+                border: InputBorder.none,
+              ),
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 18),
+              onChanged: (text) => section.name = text,
+            ),
+          ),
+          CustomIconButton(
+              iconData: Icons.delete,
+              color: Colors.white,
+              onTap: () {
+                homeManager.removeSection(section);
+              })
+        ],
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Text(
+          section.name ?? "",
+          style: const TextStyle(
+              color: Colors.white, fontWeight: FontWeight.w800, fontSize: 18),
+        ),
+      );
+    }
   }
 }
