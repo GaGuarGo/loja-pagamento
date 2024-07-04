5dc11ffa1b2d1c64cfa755a19cde4c90e2c0eccf
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-04 17:40:14 -0300

    Verificando Estoque e Decrementando

--- lib/models/checkout_manager.dart
+++ lib/models/checkout_manager.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_manager.dart';
+import 'package:loja_virtual/models/product.dart';
 
 class CheckoutManager extends ChangeNotifier {
   CartManager? cartManager;
@@ -13,10 +14,12 @@ class CheckoutManager extends ChangeNotifier {
 
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  void checkout() {
-    _decrementStock();
-
-    _getOrderId().then((value) => print(value));
+  Future<void> checkout() async {
+    try {
+      await _decrementStock();
+    } catch (e, s) {
+      log("Erro ao Finalizar Pedido", error: e, stackTrace: s);
+    }
   }
 
   Future<int> _getOrderId() async {
@@ -36,5 +39,48 @@ class CheckoutManager extends ChangeNotifier {
     }
   }
 
-  void _decrementStock() {}
+  Future<void> _decrementStock() async {
+    return firestore.runTransaction((t) async {
+      final List<Product> productsToUpdate = [];
+      final List<Product> productsWithoutStock = [];
+
+      for (final cartProduct in cartManager!.items) {
+        Product product;
+
+        if (productsToUpdate.any((p) => p.id == cartProduct.productId)) {
+          product =
+              productsToUpdate.firstWhere((p) => p.id == cartProduct.productId);
+        } else {
+          final doc =
+              await t.get(firestore.doc('products/${cartProduct.productId}'));
+
+          product = Product.fromDocument(doc);
+        }
+
+        //Atulizando o estoque disponivel no carrinho
+        cartProduct.product = product;
+
+        final size = product.findSize(cartProduct.size!);
+
+        if (size!.stock! - cartProduct.quantity! < 0) {
+          productsWithoutStock.add(product);
+        } else {
+          size.stock = size.stock! - cartProduct.quantity!;
+          productsToUpdate.add(product);
+        }
+      }
+
+      //Verificando se há produtos na transação sem estoque
+      if (productsWithoutStock.isNotEmpty) {
+        return Future.error(
+            '${productsWithoutStock.length} Produto(s) sem Estoque');
+      }
+
+      //Atualizando o estoque dos produtos
+      for (final product in productsToUpdate) {
+        t.update(firestore.doc('products/${product.id}'),
+            {'sizes': product.exportSizeList()});
+      }
+    });
+  }
 }
