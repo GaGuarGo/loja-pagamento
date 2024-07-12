e321177b3eb9208648d1ce4092443a2c06fdb7f9
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-12 15:47:27 -0300

    Criando o Objeto da Loja

--- lib/models/stores_manager.dart
+++ lib/models/stores_manager.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/store.dart';
 
 class StoresManager extends ChangeNotifier {
   StoresManager() {
@@ -8,10 +9,12 @@ class StoresManager extends ChangeNotifier {
 
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  List<Store> stores = [];
+
   Future<void> _loadStoreList() async {
     final snapshot = await firestore.collection('stores').get();
 
-    print(snapshot.docs.first.data());
+    stores = snapshot.docs.map((e) => Store.fromDocument(e)).toList();
     notifyListeners();
   }
 }
