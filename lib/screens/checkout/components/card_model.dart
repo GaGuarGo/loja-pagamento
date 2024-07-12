3ecc33cbc77a862c44b79535bbd9e4922820da1b
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-12 17:57:08 -0300

    Modelo do Cartão

---
+++ lib/screens/checkout/components/card_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CardModal extends StatelessWidget {
+  final Widget content;
+  const CardModal({super.key, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 16.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Container(
+        height: 200,
+        color: const Color(0xFF1B4B52),
+        child: content,
+      ),
+    );
+  }
+}
