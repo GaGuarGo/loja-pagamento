a06c3360753963da0d34ea99f0037271ee3128d6
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-15 18:10:55 -0300

    Criando uma Função Hello World

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,15 +5,14 @@
  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * Rodar npx eslint --ext .js,.ts . --fix antes de fazer deploy, caso haja erros
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onCall((data, context) => {
+  return {data: "Hello from Cloud Functions"};
+});
