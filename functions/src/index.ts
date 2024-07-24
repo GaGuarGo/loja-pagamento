24b2c978bcbe62d2e941e84a6689467e2c024da4
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-24 12:54:50 -0300

    Exibindo Notificação de Novo Pedido - Parte 2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -308,7 +308,11 @@ export const onNewOrder = functions.firestore.document("/orders/{orderId}")
       adminsTokens = adminsTokens.concat(tokensAdmin);
     }
 
-    console.log(orderId, adminsTokens);
+    await sendPushFCM(
+      adminsTokens,
+      "Novo Pedido",
+      "Nova Venda Realizada. Pedido: " + orderId,
+    );
   });
 
 
@@ -318,5 +322,19 @@ async function getDeviceTokens(uid: string): Promise<string[]> {
 
   const tokens = querySnapshot.docs.map((doc) => doc.id);
   return tokens;
+}
+
+async function sendPushFCM(tokens: string[], title: string, message: string) {
+  if (tokens.length > 0) {
+    const payLoad = {
+      notification: {
+        title: title,
+        body: message,
+        click_action: "FLUTTER_NOTIFICATION_CLICK",
+      },
+    };
+    return admin.messaging().sendToDevice(tokens, payLoad);
+  }
+  return;
 }
 
