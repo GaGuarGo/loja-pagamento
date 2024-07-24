16aef549bfc07b176f2d2e87529c810f97df160b
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-07-24 15:49:32 -0300

    Exibindo Notificação Interna

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,3 +1,5 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:loja_virtual/models/page_manager.dart';
@@ -11,6 +13,11 @@ import 'package:loja_virtual/screens/stores/store_screen.dart';
 import 'package:provider/provider.dart';
 
 // ignore: use_key_in_widget_constructors
+
+Future<void> onBackgroundMessage(RemoteMessage message) async {
+  print(message.notification!.title.toString());
+}
+
 class BaseScreen extends StatefulWidget {
   @override
   State<BaseScreen> createState() => _BaseScreenState();
@@ -23,6 +30,39 @@ class _BaseScreenState extends State<BaseScreen> {
   void initState() {
     super.initState();
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    configFCM();
+  }
+
+  void configFCM() {
+    final fcm = FirebaseMessaging.instance;
+
+    fcm.requestPermission(provisional: true);
+
+    FirebaseMessaging.onMessage.listen((notification) {
+      showNotification(
+          notification.notification!.title!, notification.notification?.body ?? "");
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {});
+    FirebaseMessaging.onBackgroundMessage(onBackgroundMessage);
+  }
+
+  void showNotification(String title, String message) {
+    Flushbar(
+      title: title,
+      message: message,
+      titleColor: Theme.of(context).primaryColor,
+      messageColor: Theme.of(context).primaryColor,
+      flushbarPosition: FlushbarPosition.TOP,
+      flushbarStyle: FlushbarStyle.GROUNDED,
+      isDismissible: true,
+      backgroundColor: Colors.white,
+      duration: Duration(seconds: 5),
+      icon: Icon(
+        Icons.shopping_cart_rounded,
+        color: Theme.of(context).primaryColor,
+      ),
+    ).show(context);
   }
 
   @override
